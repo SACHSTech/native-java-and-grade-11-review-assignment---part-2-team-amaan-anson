e3c42ab1b6c765fed5a1a33efb6ed001f6c8d369
Amaan Ali <>
2021-05-09 19:46:10 +0000

    completed methods1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -3,7 +3,57 @@ import java.io.*;
 
 public class Utility {
 
-    
+    package gr11review.part2;
+
+public class Utility {
+
+    public static String zipZap(String Stringstr){
+        Stringstr = Stringstr.replaceAll("zip","zp")
+                .replaceAll("zap","zp").replaceAll("zop","zp");
+        return Stringstr;
+
+    }
+
+    public static int sumNumbers(String str){
+        return 0;
+    }
+
+    public static String longestWord(String filenametxt){
+        return "yes";
+    }
+
+    public static String alphaWord(String filenametxt){
+        return "yes";
+    }
+
+
+    public static int[] tenRun(int[] nums){
+        return nums;
+
+    }
+
+    public static int[] notAlone(int[] nums, int value){
+        return nums;
+    }
+
+    public static boolean linearIn(int[] outer, int[] inner){
+        return false;
+    }
+
+    public static boolean canBalance(int[] nums){
+        return true;
+    }
+
+    public static void pascalTri(int i, int j){
+
+    }
+    public static void diagonal(int n){
+
+    }
+
+
+}
+
 
     
 }
