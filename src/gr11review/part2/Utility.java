5f17987f3a9003311683fb1fc6d692bbc0bd1cc0
Amaan Ali <>
2021-05-09 22:10:53 +0000

    Finished FileIO1 & added comments to methods 1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,59 +1,35 @@
 package gr11review.part2;
-import java.io.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Utility {
 
-    package gr11review.part2;
+    //Method 1
 
-public class Utility {
-
-    public static String zipZap(String Stringstr){
-        Stringstr = Stringstr.replaceAll("zip","zp")
-                .replaceAll("zap","zp").replaceAll("zop","zp");
-        return Stringstr;
-
+    public static String zipZap(String str){
+        //replace middle word using .replaceAll
+        str = str.replaceAll("zip","zp")
+        .replaceAll("zap","zp").replaceAll("zop","zp");
+        return str;
     }
 
-    public static int sumNumbers(String str){
-        return 0;
+    //FileIO1
+
+    public static String longestWord(String filenametxt) throws FileNotFoundException {
+        //initialize variables
+        String wordCurrent = "";
+        String wordLongest = "";
+        //setup scanner and create file
+        Scanner scanner = new Scanner (new File (filenametxt));
+            while(scanner.hasNext()) {
+                wordCurrent = scanner.next();
+                if (wordCurrent.length() > wordLongest.length()) {
+                    wordCurrent = wordLongest;
+                }
+            }
+
+        return wordLongest;
     }
-
-    public static String longestWord(String filenametxt){
-        return "yes";
-    }
-
-    public static String alphaWord(String filenametxt){
-        return "yes";
-    }
-
-
-    public static int[] tenRun(int[] nums){
-        return nums;
-
-    }
-
-    public static int[] notAlone(int[] nums, int value){
-        return nums;
-    }
-
-    public static boolean linearIn(int[] outer, int[] inner){
-        return false;
-    }
-
-    public static boolean canBalance(int[] nums){
-        return true;
-    }
-
-    public static void pascalTri(int i, int j){
-
-    }
-    public static void diagonal(int n){
-
-    }
-
-
-}
-
-
     
-}
